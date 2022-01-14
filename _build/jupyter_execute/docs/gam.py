91326bca1fc95a465476abadcb3c73750c3290fe
Jan Kirenz <jan.kirenz@outlook.de>
2022-01-14 22:13:26 +0100

    corrected GAM

--- _build/jupyter_execute/docs/gam.py
+++ _build/jupyter_execute/docs/gam.py
@@ -3,9 +3,11 @@
 
 # # Generalized Additive Models (GAM)
 # 
-# *The following code tutorial is mainly based on the [statsmodels documentation](https://www.statsmodels.org/v0.10.0/gam.html) about generalized additive models (GAM). To learn more about this method, review ["An Introduction to Statistical Learning"](https://www.statlearning.com/) from James et al. (2021).*
+# *The following code tutorial is mainly based on the [statsmodels documentation](https://www.statsmodels.org/v0.10.0/gam.html) about generalized additive models (GAM). To learn more about this method, review ["An Introduction to Statistical Learning"](https://www.statlearning.com/) from James et al. (2021). GAMs were originally developed by Trevor Hastie and Robert Tibshirani (who are two coauthors of {cite}`James2021`) to blend properties of generalized linear models with additive models.*
 # 
-# Generalized additive models allow us to use regression splines (and methods like smoothing splines and local regression) to deal with multiple predictors. In [](splines.ipynb), we discussed regression splines, which we create by specifying a set of knots, producing a sequence of basis functions, and then using least squares to estimate the spline coefficients.
+# Generalized additive models allow us to use regression splinee, smoothing splines and local regression to deal with multiple predictors. In [](splines.ipynb), we discussed regression splines, which we created by specifying a set of knots, producing a sequence of basis functions, and then using least squares to estimate the spline coefficients. 
+# 
+# In this tutorial, we use a Generalized Additive Model with a reguralized estimation of [smooth components using B-Splines](https://www.statsmodels.org/stable/generated/statsmodels.gam.smooth_basis.BSplines.html#statsmodels.gam.smooth_basis.BSplines). We could also use additive smooth components using [cyclic cubic regression splines](https://www.statsmodels.org/stable/generated/statsmodels.gam.smooth_basis.CyclicCubicSplines.html#statsmodels.gam.smooth_basis.CyclicCubicSplines).
 
 # ## Data preparation
 # 
@@ -59,20 +61,22 @@ sns.scatterplot(x='Hits', y='Salary', data=df_train);
 # 
 # As an example: 
 # 
-# - A cubic spline (degree=3) with 4 knots (K=4) will have $df=4+3=7$ degrees of freedom. If we use an intercept, we need to add an aditional degree of freedom.
-# - A cubic spline (degree=3) with 5 degrees of freedom will have $𝑘=5−3=2$ knots.
+# - A cubic spline (degree=3) with 4 knots (K=4) will have $df=4+3=7$ degrees of freedom. If we use an intercept, we need to add an additional degree of freedom.
+# - A cubic spline (degree=3) with 5 degrees of freedom (df=5) will have $𝑘=5−3=2$ knots (assuming the spline has no intercept).
 # 
-# In our case, we want to fit a cubic spline (degree=3) with an intercept and three knots (K=3) for both features (we also could use different values). This equals $df=3+3+1=7$; these degrees of freedom are used up by an intercept, plus six basis functions.
+# In our case, we want to fit a cubic spline (degree=3) with an intercept and three knots (K=3). We use this values for both of our features (we also could use different values for one of the features). This equals $df=3+3+1=7$ for both of the features. This means that these degrees of freedom are used up by an intercept, plus six basis functions.
 # 
-# The higher the degrees of freedom, the "wigglier" the spline gets because the number of knots is increased.
+# :::{Note}
+# The higher the degrees of freedom, the "wigglier" the spline gets because the number of knots is increased {cite:p}`James2021`.
+# :::
 # 
-# *In the case of natural (restricted) cubic splines: df=𝑘−1 if you specify the knots or 𝑘=df+1 if you specify the degrees of freedom.*
+# In the case of natural splines: $df=𝑘−1$ if you specify the knots or $𝑘=df+1$ if you specify the degrees of freedom.
 # 
 # In the statsmodels function `BSplines`, we need to provide `df` and `degree`:
 # 
 # `df`: number of basis functions or degrees of freedom; should be equal in length to the number of columns of x; may be an integer if x has one column or is 1-D.  
 # 
-# `degree`: degree(s) of the spline; the same length and type rules apply as to df
+# `degree`: degree(s) of the spline; the same length and type rules apply as to df.
 
 # In[6]:
 
@@ -105,7 +109,7 @@ gam_bs = GLMGam.from_formula('Salary ~ CRuns + Hits',
                                 alpha=alpha)
 
 
-# Note that optimal penalization weights alpha could be obtained through generalized k-fold cross-validation with [.select_penweight_kfold](https://www.statsmodels.org/dev/generated/statsmodels.gam.generalized_additive_model.GLMGam.select_penweight_kfold.html)
+# Note that optimal penalization weights alpha could be obtained through generalized k-fold cross-validation by using the function [select_penweight_kfold](https://www.statsmodels.org/dev/generated/statsmodels.gam.generalized_additive_model.GLMGam.select_penweight_kfold.html).
 
 # In[9]:
 
@@ -140,7 +144,6 @@ res_bs.plot_partial(1, cpr=True)
 # ## First evaluation
 
 # In[12]:
-
 
 
 df_train['y_pred'] = res_bs.predict()
