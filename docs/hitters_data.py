3e237dde951ae27f3d8f692dc98ec7e4476251a5
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2022-04-23 13:21:35 +0200

    Update hitters_data.py

--- docs/hitters_data.py
+++ docs/hitters_data.py
@@ -1,26 +1,38 @@
-# # Hitters data preparation
+# Hitters data preparation
 
-# ## Import
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
+# Import
 df = pd.read_csv("https://raw.githubusercontent.com/kirenz/datasets/master/Hitters.csv")
+
 # drop missing cases
 df = df.dropna()
-# ## Create label and features
-# Since we will use algorithms from scikit learn, we need to encode our categorical features as one-hot numeric features (dummy variables):
+
+# Create dummies
 dummies = pd.get_dummies(df[['League', 'Division','NewLeague']])
-# Next, we create our label y:
+
+# Create our label y:
 y = df['Salary']
 X_numerical = df.drop(['Salary', 'League', 'Division', 'NewLeague'], axis=1).astype('float64')
-# Make a list of all numerical features (we need them later):
+
+# Make a list of all numerical features
 list_numerical = X_numerical.columns
+
 # Create all features
 X = pd.concat([X_numerical, dummies[['League_N', 'Division_W', 'NewLeague_N']]], axis=1)
-# ### Split data
-from sklearn.model_selection import train_test_split
+
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
 
+# Data standardization
+scaler = StandardScaler().fit(X_train[list_numerical]) 
 
+X_train[list_numerical] = scaler.transform(X_train[list_numerical])
+X_test[list_numerical] = scaler.transform(X_test[list_numerical])
+
+# Make pandas dataframes
 df_train = pd.DataFrame(X_train)
 df_train['Salary'] = pd.DataFrame(y_train)
 
